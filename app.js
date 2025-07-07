d250c1877695321bfc4371068551eaf726d83dde
node-teacher <aditeacherbachir@gmail.com>
2025-07-07 18:47:57 +0300

    move to product router

--- app.js
+++ app.js
@@ -2,20 +2,20 @@
 // npm i express
 
 // 2. ES6 ייבוא של
-import express from 'express';
-
-// const {x} = require('y')
-// import {x} from 'y'
-const products = [
-    { id: 10, name: "aaa", price: 12 },
-    { id: 11, name: "bbb", price: 1000 },
-    { id: 12, name: "mmm", price: 12 },
-    { id: 14, name: "aaa", price: 30 },
-];
-
+import express, { json, urlencoded } from 'express';
+// ייבוא של כל הראוטרים
+import productRouter from './routes/products.router.js';
 
 // 3. יצירת השרת
 const app = express();
+
+// כל השרת מקבל באדי
+// כל סוגי הבקשות
+// כל הכתובות
+// כל הבקשות שיופיעו אחרי 2 השורות הבאות יקבלו משתנה נוסף לתוכן
+// req.body - אוביקט עם כל התוכן שנשלח בבאדי מהלקוח
+app.use(json());
+app.use(urlencoded());
 
 // 3.1 הגדרת הכתובות של השרת - endpoints
 
@@ -24,47 +24,14 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-app.get('/products', (req, res) => {
-    // res.send - יכול להזחיר מידע מכל טיפוס ולא רק מחרוזת
-    // res.send(products);
-
-    // res.json - שליחת אוביקט ללקוח
-    // בד"כ נשתמש בו
-    res.json(products);
-});
-
-app.post('/products', (req, res) => {
-    const p = { id: Date.now(), name: 'new', price: Math.random() * 100 };
-    products.push(p);
-
-    // res.status(...) - פונקציה שמגדירה סטטוס לתגובה
-    // json/send חובה לכתוב לפני ההחזרה
-    return res.status(201).json(p);
-});
-
-// http://loalhost:5000/products/10
-// http://loalhost:5000/products/11
-// http://loalhost:5000/products/200
-
-// :id - יוצר משתנה בשם איי-די ששומר את הערך שנשלח ביו-אר-אל
-app.put('/products/:id', (req, res) => {
-    // req.params - URL-אוביקט שמכיל את כל המפתחות והערכים שנשלחו ב
-    // הערכים נשלחים תמיד כמחרוזת
-
-    const id = +req.params.id; // + המרת מחרוזת למספר ע"י
-
-    const product = products.find(p => p.id === id);
-
-    if (!product) // לא מצא מוצר עם קוד כזה
-    {
-        return res.status(404).json({ error: 'product not found' })
-    }
-
-    product.name += ' updated';
-    product.price = 100 + Math.random() * 10;
-
-    return res.json(product);
-});
+// =========== כל הניתובים ==============
+// app.use(...) - מחפש התאמה של תחילת הניתוב
+// app.method(...) - מחפש התאמה מלאה - כל הניתוב
+// http://localhost:5000/products
+// http://localhost:5000/products/123
+// http://localhost:5000/products/abc
+// http://localhost:5000/products/full
+app.use('/products', productRouter);
 
 // 4. הרצת השרת על הפורט שהגדרנו
 const port = 5000;
---
+++ routes/products.router.js
@@ -0,0 +1,136 @@
+// ================= routes - ניתובים ==================
+// כאן יהיו הכתובות של המשאב המתאים
+// והפונקציה עצמה - הלוגיקה תהיה בתוך הקונטרולר
+
+// 1. import - חובה בתחילת העמוד
+import { Router } from 'express'
+
+let products = [
+    { id: 10, name: "aaa", price: 12 },
+    { id: 11, name: "bbb", price: 1000 },
+    { id: 12, name: "mmm", price: 12 },
+    { id: 14, name: "aaa", price: 30 },
+];
+
+// 2. יצירת ראוטר חדש - למשאב בתוך הפרויקט
+// כאן לא כותבים את הניתוב הכללי
+// רק את הניתוב החלקי אחרי שם המשאב - מתחיל בסלאש
+const router = Router();
+
+// /products
+// /products?sort=name
+// /products?sort=price
+// /products?sort=id
+router.get('/', (req, res) => {
+    // req.query - פרמטרים אופציונליים
+    // const sort = req.query.sort;
+    const { sort } = req.query;
+
+    // מיון על מערך חדש
+    let newP;
+    switch (sort) {
+        case 'price':
+            newP = products.toSorted((p1, p2) => p1.price - p2.price);
+            break;
+        case 'id':
+            newP = products.toSorted((p1, p2) => p1.id - p2.id);
+            break;
+        case 'name':
+            newP = products.toSorted((p1, p2) => p1.name.localeCompare(p2.name));
+            break;
+
+        default:
+            newP = products;
+            break;
+    }
+
+    // res.json - שליחת אוביקט ללקוח
+    // בד"כ נשתמש בו
+    res.json(newP);
+});
+
+router.get('/:id', (req, res) => {
+    const id = +req.params.id;
+    const p = products.find(p => p.id === id);
+
+    // עדיף לטפל קודם כל בשגיאות
+    if (!p) {
+        res.status(404).json({ message: 'product not found' });
+    }
+    else {
+        res.json(p);
+    }
+});
+
+router.post('/', (req, res) => {
+    // console.log(req.body); // כל מה שנשלח בבאדי
+
+    // const p = { id: Date.now(), name: req.body.name, price: req.body.price };
+    const p = {
+        id: Date.now(),
+        ...req.body, // name: req.body.name, price: req.body.price 
+    };
+
+    products.push(p);
+
+    // res.status(...) - פונקציה שמגדירה סטטוס לתגובה
+    // json/send חובה לכתוב לפני ההחזרה
+    return res.status(201).json(p);
+});
+
+// http://loalhost:5000/products/10
+// http://loalhost:5000/products/11
+// http://loalhost:5000/products/200
+
+// :id - יוצר משתנה בשם איי-די ששומר את הערך שנשלח ביו-אר-אל
+router.put('/:id', (req, res) => {
+    // req.params - URL-אוביקט שמכיל את כל המפתחות והערכים שנשלחו ב
+    // הערכים נשלחים תמיד כמחרוזת
+
+    const id = +req.params.id; // + המרת מחרוזת למספר ע"י
+
+    const product = products.find(p => p.id === id);
+
+    if (!product) // לא מצא מוצר עם קוד כזה
+    {
+        return res.status(404).json({ error: 'product not found' })
+    }
+
+    // עדכון רק אם נשלח הערך - default value
+    // product.name =  req.body.name ? req.body.name : product.name;
+    product.name =  req.body.name || product.name;  // false -> 0/''/false/undefined/null
+    product.price = req.body.price ?? product.price; // false -> undefined/null
+
+    return res.json(product);
+});
+
+router.delete('/:idx', (req, res) => {
+    const id = +req.params.idx;
+
+    const p = products.find(p => p.id === id);
+
+    // עדיף לטפל קודם כל בשגיאות
+    if (!p) {
+        res.status(404).json({ message: 'product not found' });
+    }
+    else {
+        products = products.filter(p => p.id !== id);
+        res.status(204).json();
+    }
+});
+
+/*
+    const sortedValues = values.toSorted(פונקצית השוואה בין 2 ערכים);
+    הפונקציה מחזירה אחד מבין הערכים הבאים
+    חיובי - אם הראשון גדול מהשני
+    שלילי - אם הראשון קטן מהשני
+    אפס - אם 2 הערכים שווים
+
+    const values = [10, 10, 21, 2];
+    const sortedValues = values.toSorted((a, b) => a - b);
+    a - b = 10 - 10 = 0   (first = second)
+    a - b = 10 - 21 = -11 (first < second)
+    a - b = 21 - 2  = 19  (first > second)
+*/
+
+export default router;
